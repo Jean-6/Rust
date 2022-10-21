f03cb8dc29fda1d7706cbdc715bd1a9f4545f25d
oka.jeanjaures@gmail.com <oka.jeanjaures@gmail.com>
2022-10-21 14:49:15 +0200

    first quiz

---
+++ move_semantics/mod.rs
@@ -0,0 +1,9 @@
+mod move_semantics1;
+mod move_semantics2;
+mod move_semantics3;
+mod move_semantics4;
+mod move_semantics5;
+mod move_semantics6;
+mod move_semantics7;
+mod move_semantics8;
+mod move_semantics9;
---
+++ move_semantics/move_semantics6.rs
@@ -0,0 +1,21 @@
+// move_semantics6.rs
+// Make me compile! `rustlings hint move_semantics6` for hints
+// You can't change anything except adding or removing references
+
+
+fn main() {
+    let data = "Rust is great!".to_string();
+    get_char(data.clone());
+    string_uppercase(data.clone());
+}
+
+// Should not take ownership
+fn get_char(data: String) -> char {
+    data.chars().last().unwrap()
+}
+
+// Should take ownership
+fn string_uppercase(mut data: String) {
+    data = data.to_uppercase();
+    println!("{}", data);
+}
