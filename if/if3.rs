a475a06c548fb11915a7fcb0255c8eeebc7795c5
oka.jeanjaures@gmail.com <oka.jeanjaures@gmail.com>
2022-10-21 14:45:29 +0200

    conditions

---
+++ if/if3.rs
@@ -0,0 +1,33 @@
+// if3.rs
+// Step 1: Make me compile!
+// Step 2: Get the tests passing!
+// Execute `rustlings hint if3` for hints!
+
+
+
+fn compute_fees(price: i32) -> i32 {
+    let fees: i32; // (neither mut nor initialization allowed here)
+    // * fees for price price lower than 10 is 1
+    // * fees for price higher than 10 are 10% rounded down (use / 10)
+    // don't change before that line
+    let mut fees=0;
+    if price < 10 {
+        fees = 1;
+    }else if price >=10 {
+        fees =(price*10)/100;
+    }
+    // don't change after that line
+    fees
+}
+
+// Don't modify this function!
+#[test]
+fn verify_fees() {
+    assert_eq!(1, compute_fees(2));
+    assert_eq!(1, compute_fees(10));
+    assert_eq!(1, compute_fees(19));
+    assert_eq!(4, compute_fees(42));
+}
+
+
+
