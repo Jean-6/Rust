3731ef15eab04806e0e065c77b5b02dcfc2168d3
oka.jeanjaures@gmail.com <oka.jeanjaures@gmail.com>
2022-10-21 14:41:50 +0200

    variables exercises

---
+++ variables/variables1.rs
@@ -0,0 +1,10 @@
+// variables1.rs
+// Make me compile!
+// Execute the command `rustlings hint variables1` if you want a hint :)
+
+const x :i32= 5;
+
+fn main() {
+    
+    println!("x has the value {}", x);
+}
