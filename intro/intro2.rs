a690e41b9b1b362d803be4c5a79f174d15929bd0
oka.jeanjaures@gmail.com <oka.jeanjaures@gmail.com>
2022-10-21 14:18:34 +0200

    intro exercise

---
+++ intro/intro2.rs
@@ -0,0 +1,14 @@
+// intro2.rs
+// Make the code print a greeting to the world.
+// Execute `rustlings hint intro2` for a hint.
+
+const x:&str="world";
+//static x: &'static str = "world";
+
+
+fn main() {
+
+    
+
+    println!("Hello {} !",x);
+}
